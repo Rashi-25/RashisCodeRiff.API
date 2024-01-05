bb145d3a536514118787215337ec8db4ed7e511c
Rashi-25 <it.rashi25@gmail.com>
2024-01-05 23:03:47 +0530

    Add local Host

--- RashisCodeRiff.API/RashisCodeRiff.API/Program.cs
+++ RashisCodeRiff.API/RashisCodeRiff.API/Program.cs
@@ -29,6 +29,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseCors(options =>
 {
+    options.WithOrigins("http://localhost:4200");
     options.AllowAnyHeader();
     options.AllowAnyOrigin();
     options.AllowAnyMethod();
