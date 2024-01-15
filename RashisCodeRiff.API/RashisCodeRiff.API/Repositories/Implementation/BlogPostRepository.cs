ca08a585616654238e7e9fece36c5663e06dc380
Rashi-25 <it.rashi25@gmail.com>
2024-01-15 19:50:26 +0530

    Edit or Update blog post using PUT API.

--- RashisCodeRiff.API/RashisCodeRiff.API/Repositories/Implementation/BlogPostRepository.cs
+++ RashisCodeRiff.API/RashisCodeRiff.API/Repositories/Implementation/BlogPostRepository.cs
@@ -29,5 +29,25 @@ namespace RashisCodeRiff.API.Repositories.Implementation
         {
             return await dbContext.BlogPosts.Include(x => x.Categories).FirstOrDefaultAsync(x => x.Id == id);
         }
+        public async Task<BlogPost?> UpdateAsync(BlogPost blogPost)
+        {
+            var existingBlogPost = await dbContext.BlogPosts.Include(x => x.Categories)
+                .FirstOrDefaultAsync(x => x.Id == blogPost.Id);
+
+            if (existingBlogPost == null)
+            {
+                return null;
+            }
+
+            // Update BlogPost
+            dbContext.Entry(existingBlogPost).CurrentValues.SetValues(blogPost);
+
+            // Update Categories
+            existingBlogPost.Categories = blogPost.Categories;
+
+            await dbContext.SaveChangesAsync();
+
+            return blogPost;
+        }
     }
 }
