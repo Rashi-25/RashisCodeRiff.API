80126e51f9903f9da91127029083b164eda8b520
Rashi-25 <it.rashi25@gmail.com>
2024-01-21 13:46:36 +0530

    AuthDbContetFile Database

---
+++ RashisCodeRiff.API/RashisCodeRiff.API/Data/AuthDbContext.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace RashisCodeRiff.API.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "93f7cabb-35fd-4794-b7f1-95a38681800e";
+            var writerRoleId = "80141906-0805-4bad-9362-0cc87c738b6b";
+
+            // Create Reader and Writer Role
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Id = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper(),
+                    ConcurrencyStamp = readerRoleId
+                },
+                new IdentityRole()
+                {
+                    Id = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper(),
+                    ConcurrencyStamp = writerRoleId
+                }
+            };
+
+            // Seed the roles
+            builder.Entity<IdentityRole>().HasData(roles);
+
+
+            // Create an Admin User
+            var adminUserId = "05909658-11c8-4c03-851d-cf47f8d73681";
+            var admin = new IdentityUser()
+            {
+                Id = adminUserId,
+                UserName = "admin@rashiscoderiff.com",
+                Email = "admin@rashiscoderiff.com",
+                NormalizedEmail = "admin@rashiscoderiff.com".ToUpper(),
+                NormalizedUserName = "admin@rashiscoderiff.com".ToUpper()
+            };
+
+            admin.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(admin, "Admin@123");
+
+            builder.Entity<IdentityUser>().HasData(admin);
+
+            // Give Roles To Admin
+
+            var adminRoles = new List<IdentityUserRole<string>>()
+            {
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = readerRoleId
+                },
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = writerRoleId
+                }
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+        }
+    }
+}
