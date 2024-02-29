a3c55eef6560b81c08a911c703243b0751904746
Rashi-25 <it.rashi25@gmail.com>
2024-02-29 19:19:48 +0530

    Changes and updates for prod

--- RashisCodeRiff.API/RashisCodeRiff.API/Program.cs
+++ RashisCodeRiff.API/RashisCodeRiff.API/Program.cs
@@ -79,11 +79,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseCors("angularApplication");
